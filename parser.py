30282019a53395d2340b686e05521cff3e7087e7
Vincent Jacques <vincent@vincent-jacques.net>
2013-10-19 17:39:37 -0700

    Refactor

--- parser.py
+++ parser.py
@@ -49,54 +49,115 @@ class ParsingError(Exception):
     pass
 
 
+class Cursor(object):
+    def __init__(self, s):
+        self.__s = s
+        self.__i = 0
+
+    def get(self, n):
+        self.__assertNMoreChars(n)
+        return self.__get(n)
+
+    def __assertNMoreChars(self, n):
+        assert self.__i + n <= len(self.__s)
+
+    def __get(self, n):
+        return self.__s[self.__i:self.__i + n]
+
+    def advance(self, n):
+        v = self.get(n)
+        self.__i += n
+        return v
+
+    def startswith(self, s):
+        return self.__get(len(s)) == s
+
+    def consume(self, s):
+        assert self.startswith(s)
+        self.advance(len(s))
+
+    @property
+    def finished(self):
+        return self.__i == len(self.__s)
+
+    @property
+    def position(self):
+        return self.__i
+
+
+class CursorTestCase(unittest.TestCase):
+    def setUp(self):
+        self.c = Cursor("abcde")
+
+    def testAdvanceToEnd(self):
+        self.assertEqual(self.c.advance(4), "abcd")
+        self.assertFalse(self.c.finished)
+        self.assertEqual(self.c.advance(1), "e")
+        self.assertTrue(self.c.finished)
+
+    def testConsumeToEnd(self):
+        self.c.consume("abcd")
+        self.assertFalse(self.c.finished)
+        self.assertEqual(self.c.position, 4)
+        self.assertEqual(self.c.get(1), "e")
+        self.c.consume("e")
+        self.assertTrue(self.c.finished)
+
+
 def parse(s):
-    ok, remaining, exprOrError = parseStringExpr(s)
+    c = Cursor(s)
+    expr = expect(parseStringExpr, c)
+    assert c.finished
+    return expr
+
+def expect(parse, c):
+    ok, exprOrError = parse(c)
     if ok:
-        assert remaining == "", remaining
         return exprOrError
     else:
-        raise ParsingError(s[:len(s) - len(remaining)], remaining, exprOrError)
+        raise ParsingError(c.position, exprOrError)
 
-def parseStringExpr(s):
-    return parseString(s)
+def parseStringExpr(c):
+    return parseString(c)
 
-def parseString(s):
-    if s.startswith('"'):
-        s = s[1:]
+def parseString(c):
+    # if c.startswith('"'):
+        c.consume('"')
         elements = []
-        while not s.startswith('"'):
-            ok, s, element = acceptStringElement(s)
-            if not ok:
-                return False, s, element
-            elements.append(element)
-        assert s.startswith('"')
-        s = s[1:]
-        return True, s, String(elements)
+        while not c.startswith('"'):
+            if c.finished:
+                return False, "Hit EOF while parsing string"
+            elements.append(expect(parseStringElement, c))
+        c.consume('"')
+        return True, String(elements)
+    # else:
+        # return False, "Expected '\"'"
 
-def acceptStringElement(s):
-    if s.startswith("\\"):
-        s = s[1:]
-        if s[0] in ('"', '\\'):
-            return True, s[1:], Escape(s[0])
-        return False, s, "Expected '\"' or '\\'"
-    if len(s) == 0:
-        return False, s, "Hit EOF while parsing string"
+def parseStringElement(c):
+    if c.startswith("\\"):
+        c.consume("\\")
+        if c.get(1) in ('"', '\\'):
+            return True, Escape(c.advance(1))
+        return False, "Expected '\"' or '\\'"
+    # elif c.startswith('"'):
+    #     return False, "Unexpected '\"'"
     else:
-        assert s[0] != '"'
-        return True, s[1:], Char(s[0])
+        return True, Char(c.advance(1))
 
 
 class TestCase(unittest.TestCase):
-    def parseDump(self, i, o):
-        self.assertEqual(parse(i).dump(), o)
+    def parseDump(self, input, expectedOutput):
+        actualOutput = parse(input).dump()
+        self.assertEqual(actualOutput, expectedOutput)
 
-    def expectParsingError(self, i, o, m):
+    def expectParsingError(self, input, expectedPosition, expectedMessage):
         with self.assertRaises(ParsingError) as cm:
-            parse(i)
-        a, b, message = cm.exception.args
-        self.assertEqual(a + b, i)
-        self.assertEqual(a, o)
-        self.assertEqual(message, m)
+            actualOutput = parse(input).dump()
+        exception = cm.exception
+        actualPosition, actualMessage = exception.args
+        self.assertIsInstance(exception, ParsingError)
+        self.assertEqual(actualPosition, expectedPosition)
+        self.assertEqual(actualMessage, expectedMessage)
 
     def testSimpleString(self):
         self.parseDump('"abc"', "String('a', 'b', 'c')")
@@ -105,11 +166,11 @@ class TestCase(unittest.TestCase):
         self.parseDump('"a\\"b\\\\c"', "String('a', <\">, 'b', <\\>, 'c')")
 
     def testUnterminatedString(self):
-        self.expectParsingError('"abc', '"abc', "Hit EOF while parsing string")
+        self.expectParsingError('"abc', 4, "Hit EOF while parsing string")
 
     def testBadEscapeSequence(self):
-        self.expectParsingError('"ab\\c"', '"ab\\', "Expected '\"' or '\\'")
+        self.expectParsingError('"ab\\c"', 4, "Expected '\"' or '\\'")
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # pragma no branch
     unittest.main()
