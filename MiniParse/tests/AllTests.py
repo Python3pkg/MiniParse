15b13b71f1f3887045d115b66a5ecacb4c31d820
Vincent Jacques <vincent@vincent-jacques.net>
2013-10-21 21:12:15 -0700

    Remove old code, deactivate old tests

--- MiniParse/tests/AllTests.py
+++ MiniParse/tests/AllTests.py
@@ -12,5 +12,3 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
 
 # You should have received a copy of the GNU Lesser General Public License along with MiniParse.  If not, see <http://www.gnu.org/licenses/>.
-
-from Legacy import *
